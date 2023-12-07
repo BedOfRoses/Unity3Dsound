d36fac8c0f25108d86fbe18e02fd4edd84c6cf37
BedOfRoses <50531780+BedOfRoses@users.noreply.github.com>
2023-12-07 22:41:34 +0100

    Update SoundPlayer.cs

--- Assets/SoundPlayer.cs
+++ Assets/SoundPlayer.cs
@@ -3,130 +3,116 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
 [RequireComponent(typeof(AudioSource))]
 public class SoundPlayer : MonoBehaviour
 {
-        AudioSource audioSource;
-        const int buffer= 1024;
-        float[] audioData;
-        private int currentPosition = 0;
-        [SerializeField , Header("3Dsound")]
-        float maxDistance = 200.0f;
-        [SerializeField]
-        public float orbitSpeed = 20f;
-        [SerializeField]
-        public Transform player;
-        [SerializeField]
-        float Radius = 5.0f;
-        // used awake to read and load the file 
-        void Awake()
+    // Audio-related variables
+    AudioSource audioSource;
+    const int buffer = 1024;
+    float[] audioData;
+    private int currentPosition = 0;
+
+    // Sound properties
+    [SerializeField, Header("3D sound")] float maxDistance = 200.0f;
+    [SerializeField] public float orbitSpeed = 20f;
+    [SerializeField] public Transform player;
+    [SerializeField] float Radius = 5.0f;
+
+    // Use Awake to read and load the audio file
+    void Awake()
+    {
+        // Load an audio file from a text file
+        string audioFilePath = ReadAudioFilePathFromFile();
+        audioSource = GetComponent<AudioSource>();
+
+        // 3D audio settings
+        audioSource.spatialize = true;
+        audioSource.spatialBlend = 1.0f; // Full 3D spatialization
+        audioSource.spatializePostEffects = true;
+        audioSource.maxDistance = 400.0f;
+
+        if (!string.IsNullOrEmpty(audioFilePath))
         {
-            // Load an audio file  from a text file
-            string audioFilePath = ReadAudioFilePathFromFile();
+            audioData = LoadAudioData(audioFilePath);
+            PlayAudio();
+        }
+        else
+        {
+            Debug.LogError("Failed to read audio file path.");
+        }
+    }
+
+    // Function to play loaded audio data
+    void PlayAudio()
+    {
+        if (audioData != null && audioData.Length > 0)
+        {
             audioSource = GetComponent<AudioSource>();
-            //3D settings
-            audioSource.spatialize = true;
-            audioSource.spatialBlend = 1.0f; // 1.0 means full 3D spatialization
-            audioSource.spatializePostEffects = true;
-            audioSource.maxDistance = 400.0f;
-            //GameObject player = GetComponent<GameObject>();
-            
-            if (!string.IsNullOrEmpty(audioFilePath))
-            {
-                audioData = LoadAudioData(audioFilePath);
-                PlayAudio();
-            }
-            else
-            {
-                Debug.LogError("Failed to read audio file path.");
-            }
+            audioSource.clip = AudioClip.Create("LoadedClip", audioData.Length, 1, 44100, false);
+            audioSource.clip.SetData(audioData, 0);
+            audioSource.Play(0);
         }
-
-        void PlayAudio()
+        else
         {
-            if (audioData != null && audioData.Length > 0)
-            {
-                audioSource = GetComponent<AudioSource>();
-                audioSource.clip = AudioClip.Create("LoadedClip", audioData.Length, 1, 44100, false);
-                audioSource.clip.SetData(audioData, 0);
-                audioSource.Play(0);
-            }
-            else
-            {
-                Debug.LogError("Failed to load audio data.");
-            }
+            Debug.LogError("Failed to load audio data.");
         }
+    }
 
-        float[] LoadAudioData(string filePath)
+    // Function to load audio data from a file
+    float[] LoadAudioData(string filePath)
+    {
+        try
         {
-            try
-            {
-                // Load WAV file 
-                byte[] fileBytes = System.IO.File.ReadAllBytes(filePath);
-                int headerOffset = 44; // Assuming a standard WAV header size
-                float[] audioData = new float[(fileBytes.Length - headerOffset) / 2];
-                /*
-                fileBytes.Length: This refers to the length of the byte array named fileBytes.
-                headerOffset: This is likely a variable representing the number of bytes to skip at the beginning of the fileBytes. 
-                This is common when dealing with audio file formats that have a header containing metadata information.
-                */
-                for (int i = 0; i < audioData.Length; i++)
-                { 
-                    audioData[i] = (short)(fileBytes[headerOffset + i * 2] | (fileBytes[headerOffset + i * 2 + 1] << 8)) / 32768.0f; // refer to footnotes Part1
-                }
-                //Debug.Log(audioData.Length);
-                return audioData;
-            }
-            catch (System.Exception ex)
-            {
-                Debug.LogError($"Failed to load audio data: {ex.Message}");
-                return null;
-            }
-        }
+            // Load WAV file 
+            byte[] fileBytes = System.IO.File.ReadAllBytes(filePath);
+            int headerOffset = 44; // Assuming a standard WAV header size
+            float[] audioData = new float[(fileBytes.Length - headerOffset) / 2];
 
-        string ReadAudioFilePathFromFile()
+            // Convert audio file bytes to floating-point audio samples
+            for (int i = 0; i < audioData.Length; i++)
+            {
+                // Convert bytes to short and normalize to floating-point audio samples
+                audioData[i] = (short) (fileBytes[headerOffset + i * 2] | (fileBytes[headerOffset + i * 2 + 1] << 8)) /
+                               32768.0f; // Refer to footnotes Part1
+            }
+
+            return audioData;
+        }
+        catch (System.Exception ex)
         {
-   
-            string p1 = Application.dataPath; // get the path 
-            p1 = p1.Replace("/Assets", "/Assets/Audios/myfile.wav"); // replace the app address with the following
-            Debug.Log(p1.ToString());
-            return p1.ToString();
-            
+            Debug.LogError($"Failed to load audio data: {ex.Message}");
+            return null;
         }
+    }
 
-        private void LateUpdate()
-        {
-            RotateAround();
-            Vector3 playerPosition = Camera.main.transform.position; // player position
-            Vector3 soundPosition = transform.position; // source of audio // rotating cube
-            float distance = Vector3.Distance(playerPosition, soundPosition);
-           // Debug.Log(soundPosition.ToString());
-           //audioSource.transform.position = soundPosition
-           audioSource.volume = Mathf.Clamp01(1.0f - distance / maxDistance);   //  adjust volume based on distance
-          
-        }
-        void RotateAround()
-            {
-                float angle = Time.time * orbitSpeed;
-                // Calculate the new position of the cube around the center
-                Vector3 offset = new Vector3(Mathf.Sin(angle), 0, Mathf.Cos(angle)) * Radius; // You can change radius
-                Vector3 newPosition = player.transform.position + offset;
+    // Function to read the audio file path from a file
+    string ReadAudioFilePathFromFile()
+    {
+        // Read and construct the audio file path
+        string p1 = Application.dataPath;
+        p1 = p1.Replace("/Assets", "/Assets/Audios/myfile.wav");
+        Debug.Log(p1.ToString());
+        return p1.ToString();
+    }
 
-                // Set the position of the cube
-                transform.position = newPosition;
-        
-                // Make the cube look at the center cube to face it while rotating
-                transform.LookAt(player);
-                Debug.Log("New Position: " + newPosition);
-            }
+    // LateUpdate function to adjust audio properties based on player's distance from the sound source
+    private void LateUpdate()
+    {
+        RotateAround();
+        Vector3 playerPosition = Camera.main.transform.position; // Player position
+        Vector3 soundPosition = transform.position; // Source of audio (rotating cube)
+        float distance = Vector3.Distance(playerPosition, soundPosition);
+        audioSource.volume = Mathf.Clamp01(1.0f - distance / maxDistance); // Adjust volume based on distance
+    }
+
+    // Function to orbit the sound source around the player
+    void RotateAround()
+    {
+        float angle = Time.time * orbitSpeed;
+        Vector3 offset = new Vector3(Mathf.Sin(angle), 0, Mathf.Cos(angle)) * Radius;
+        Vector3 newPosition = player.transform.position + offset;
+        transform.position = newPosition;
+        transform.LookAt(player); // Make the sound source face the player while rotating
+        Debug.Log("New Position: " + newPosition);
+    }
 }
-
-// part 1
-/*
-fileBytes[headerOffset + i * 2]: Accesses the i-th pair of bytes from the fileBytes array. The i * 2 is used to skip every other byte, assuming each audio sample is represented by two bytes.
-fileBytes[headerOffset + i * 2 + 1] << 8: Accesses the next byte and shifts its bits to the left by 8 positions. This is done to combine it with the previous byte, effectively merging two bytes into a 16-bit short.
-(short)(...): Casts the combined bytes to a 16-bit signed short. This assumes that the audio data is stored as signed 16-bit PCM (common for uncompressed audio).
-... / 32768.0f: Divides the obtained short value by 32768.0 to normalize the audio sample to the range [-1.0, 1.0]. This is a common practice when working with audio data in a floating-point representation.
-audioData[i] = ...: Assigns the normalized floating-point value to the i-th element of the audioData array.
-*/
